a189cc8d77a2c04b3a8095cec979356a33a064c3
Andrea Roncoli <115221482+DreRnc@users.noreply.github.com>
2022-12-10 16:32:05 +0100

    Added function to generate MLP

--- src/MLP.py
+++ src/MLP.py
@@ -11,3 +11,23 @@ class MLP:
     def predict(self, X):
         pass
 
+def main():
+    # Path: src/main.py
+    # Load data
+    X, y = load_data()
+    
+    # Create MLP
+    mlp = MLP()
+    
+    # Add layers
+    mlp.add_layer(Layer(2, 3, activation='relu'))
+    mlp.add_layer(Layer(3, 1, activation='sigmoid'))
+    
+    # Fit
+    mlp.fit(X, y)
+    
+    # Predict
+    y_pred = mlp.predict(X)
+    
+    # Print results
+    print(y_pred)
