c36572bf78d45183ad0d7c996dd8b1d99c37238f
biancaziliotto <b.ziliotto@studenti.unipi.it>
2022-12-11 09:27:44 +0100

    modified:   src/MLP.py

--- src/MLP.py
+++ src/MLP.py
@@ -1,21 +1,88 @@
 import numpy as np
 import math
 
-from Layers.py import layer, 
+from Layers import Dense, FullyConnected
+from Optimization_algorithms import gradient_descent
+from Functions import MSE, NoReg,  L1Reg, L2Reg
 
 class MLP:
-    def __init__(self, n_layers, units_per_layer):
+
+    """
+    MLP class:
+
+    Attributes
+    -----------
+
+    n_layers: number of hidden layers
+    layers: list of layers
+
+    batch_size = 1;
+    optimization_algorithm = gradient_descent()
+    cost_function = MSE()
+    regularization_function = NoReg()
+
+    Methods
+    --------
+
+    add_layer(new_layer): adds new layer to layers
+    predict(X): computes y_pred
+    fit(X,y_true): trains MLP on the given training set
+
+
+    """
+
+    def __init__(self, n_layers, layer_units):
+
+        """
+        Build MLP 
+
+        Parameters
+        -----------
+        n_layers : number of layers
+        layer_units : list containing the number of units for each layer
+
+        """
+
         self.n_layers = 0
         self.layers = []
         for l in range(n_layers):
-            self.add_layer(units_per_layer[l])
+            new_layer = Dense(layer_units[l-1], layer_units[l])
+            self.add_layer(new_layer)
 
-    def add_layer(self, n_units):
-        new_layer = Dense(n_units)
+        self.batch_size = 1;
+        self.optimization_algorithm = gradient_descent()
+        self.cost_function = MSE()
+        self.regularization_function = NoReg()
+
+
+    def add_layer(self, new_layer):
+
+        """
+        Add new layer to layers
+
+        Parameters
+        -----------
+        new_layer
+
+        """
         self.layers.append(new_layer)
         self.n_layers += 1
 
     def predict(self, X):
+
+        """
+        Computes the predicted outputs of the MLP 
+
+        Parameters
+        -----------
+        X : input matrix (n_samples x n_features)
+
+        Returns
+        -------
+        y_pred : output vector (n_samples)
+
+        """
+
         n_samples = X.shape[0]
         y_pred = np.empty(n_samples)
         for sample in range(n_samples):
@@ -27,17 +94,29 @@ class MLP:
         return y_pred
 
     
-    def fit(self, X, y_true, batch_size, optimization_algorithm):
+    def fit(self, X, y_true):
+
+        """
+        Fits the weigths of the MLP 
+
+        Parameters
+        -----------
+        X : input matrix (n_samples x n_features)
+        y_true : target outputs (n_samples)
+
+
+        """
+
+
         n_samples = X.shape[0]
-        n_batches = math.ceil(n_samples/batch_size)
+        n_batches = math.ceil(n_samples/self.batch_size)
         batch = 0
         
         for batch in range(n_batches):
-            y_pred = self.predict(X[batch * batch_size : (batch+1)*batch_size])
-            
+            y_pred = self.predict(X[batch * self.batch_size : (batch+1)*self.batch_size])
+            cost = self.cost_function(y_pred, y_true[batch * self.batch_size : (batch+1)*self.batch_size])
             for layer in self.layers:
-                grad_input = layer.backpropagation
-
+                grad_input = layer.backpropagation()
 
 
         
