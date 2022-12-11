bb72572cb963c7bc2e5617045e58a7e5b5e5d8bc
biancaziliotto <b.ziliotto@studenti.unipi.it>
2022-12-11 14:56:40 +0100

    modified:   src/MLP.py

--- src/MLP.py
+++ src/MLP.py
@@ -1,9 +1,9 @@
 import numpy as np
 import math
 
-from Layers import Dense, FullyConnected
-from Optimization_algorithms import gradient_descent
-from Functions import MSE, NoReg,  L1Reg, L2Reg
+from Layers import Layer, FullyConnected, Dense
+
+from Functions import get_activation_instance, get_regularization_instance, get_error_function
 
 class MLP:
 
@@ -16,20 +16,12 @@ class MLP:
     n_layers: number of hidden layers
     layers: list of layers
 
-    (questi boh vediamo)
-
-    batch_size = 1;
-    optimization_algorithm = gradient_descent()
-    cost_function = MSE()
-    regularization_function = NoReg()
-
     Methods
     --------
 
-    add_layer(new_layer): adds new layer to layers
-    predict(X): computes y_pred
-    fit(X,y_true): trains MLP on the given training set
-
+    add_layer: adds new layer to layers
+    predict: computes y_pred
+    fit: fits MLP weigths on the given training set
 
     """
 
@@ -50,11 +42,6 @@ class MLP:
         for l in range(n_layers):
             new_layer = Dense(layer_units[l-1], layer_units[l])
             self.add_layer(new_layer)
-
-        self.batch_size = 1;
-        self.optimization_algorithm = gradient_descent()
-        self.cost_function = MSE()
-        self.regularization_function = NoReg()
 
 
     def add_layer(self, new_layer):
@@ -96,7 +83,7 @@ class MLP:
         return y_pred
 
     
-    def fit(self, X, y_true):
+    def fit(self, X, y_true, batch_size, optimization_step, regularization_function_str, error_function_str):
 
         """
         (TO BE COMPLETED)
@@ -108,19 +95,31 @@ class MLP:
         X : input matrix (n_samples x n_features)
         y_true : target outputs (n_samples)
 
+        batch_size : 
+        optimization_algorithm :
+        regularization_function : 
+        cost_function :
+
         """
 
 
+        regularization_function = get_regularization_instance(regularization_function_str)
+        error_function = get_error_function(error_function_str)
+
         n_samples = X.shape[0]
         n_batches = math.ceil(n_samples/self.batch_size)
-        batch = 0
+        batches = [{"X" : X[b * batch_size : (b+1)*batch_size], \
+            "y_true" :  y_true[b * batch_size : (b+1)*batch_size]} \
+            for b in range(n_batches - 1)]
+        batches.append({"X" : X[(n_batches-1) * batch_size : -1], "y_true" : y_true[(n_batches-1) * batch_size : -1]})
         
-        for batch in range(n_batches):
-            y_pred = self.predict(X[batch * self.batch_size : (batch+1)*self.batch_size])
-            cost = self.cost_function(y_pred, y_true[batch * self.batch_size : (batch+1)*self.batch_size])
+        for batch in batches:
+            
             for layer in self.layers:
-                grad_input = layer.backpropagation()
-
+                layer.get_params["weigths"]
+                grad_weigths = layer.backprop(X[sample], y[sample])
+                new_weigths = optimization_algorithm(weigths, optimization_step, grad_weigths)
+                layer.set_params({"weigths" : new_weigths})
 
         
         return        
