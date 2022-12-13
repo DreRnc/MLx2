88e94030434e8664339761f7fa8459ae8a69c2c6
biancaziliotto <b.ziliotto@studenti.unipi.it>
2022-12-13 11:15:57 +0100

    modified:   src/MLP.py

--- src/MLP.py
+++ src/MLP.py
@@ -18,9 +18,8 @@ class MLP:
     Methods
     --------
 
-    add_layer: adds new layer to layers
-    predict: computes y_pred
     fit: fits MLP weigths on the given training set
+    predict: computes y_pred
 
     """
 
@@ -43,27 +42,59 @@ class MLP:
 
         for l in range(n_hidden_layers):
             new_layer = Dense(layer_units[l], layer_units[l-1], activation_function)
-            self.add_layer(new_layer)
+            self.layers.append(new_layer)
         
         output_layer = Fully_Connected_Layer(output_size, input_size)
 
-        self.add_layer(output_layer)
+        self.layers.append(output_layer)
 
 
-    def add_layer(self, new_layer):
+    
+    def fit(self, X, y_true, batch_size, inizialization_str, scale, error_function_str, optimizer_str, regularization_function_str):
 
         """
-        Add new layer to layers
-        
-        (Forse per ora messa così è inutile?)
+
+        Fits the weigths and biases of the MLP 
 
         Parameters
         -----------
-        new_layer
+        X : input matrix (n_samples x n_features)
+        y_true : target outputs (n_samples)
+
+        batch_size (int) 
+        regularization_function (str)
+        cost_function (str)
 
         """
-        self.layers.append(new_layer)
 
+        n_samples = X.shape[0]
+        n_batches = math.ceil(n_samples/batch_size)
+
+        for layer in self.layers:
+            layer.initialize(inizialization_str, scale, optimizer_str, regularization_function_str)
+
+        error_function = GetMetricFunction(error_function_str)
+        
+        for batch in range(n_batches):
+
+            if batch != n_batches - 1 :
+                X_batch = X[batch * batch_size : (batch+1)*batch_size]
+                y_true_batch = y_true[batch * batch_size : (batch+1)*batch_size]
+            else:
+                X_batch = X[batch * batch_size : -1]
+                y_true_batch = y_true[batch * batch_size : -1]
+
+            y_pred_batch = self.predict(X_batch)
+            print(error_function_str + " = " + str(error_function(y_true_batch, y_pred_batch)))
+            
+            grad_outputs = error_function.derivative(y_true_batch, y_pred_batch)
+            
+            for layer in self.layers:
+
+                grad_inputs = layer.backprop(grad_outputs)
+
+
+        
     def predict(self, X):
 
         """
@@ -92,50 +123,4 @@ class MLP:
                 tmp = layer.output
             y_pred[sample] = layer.output
         return y_pred
-
-    
-    def fit(self, X, y_true, batch_size, error_function_str, regularization_function_str = "noreg"):
-
-        """
-
-        Fits the weigths and biases of the MLP 
-
-        Parameters
-        -----------
-        X : input matrix (n_samples x n_features)
-        y_true : target outputs (n_samples)
-
-        batch_size (int) 
-        regularization_function (str)
-        cost_function (str)
-
-        """
-
-        n_samples = X.shape[0]
-        n_batches = math.ceil(n_samples/batch_size)
-
-        error_function = GetMetricFunction(error_function_str)
-        #regularization_function = GetRegularizationFunction(regularization_function_str)
-        
-        for batch in range(n_batches):
-
-            if batch != n_batches - 1 :
-                X_batch = X[batch * batch_size : (batch+1)*batch_size]
-                y_true_batch = y_true[batch * batch_size : (batch+1)*batch_size]
-            else:
-                X_batch = X[batch * batch_size : -1]
-                y_true_batch = y_true[batch * batch_size : -1]
-
-            y_pred_batch = self.predict(X_batch)
-            print(error_function(y_true_batch, y_pred_batch))
-            
-            grad_outputs = error_function.derivative(y_true_batch, y_pred_batch)
-            
-            for layer in self.layers:
-
-                grad_inputs = layer.backprop(grad_outputs)
-
-
-        
-    
 
